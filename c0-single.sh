4dcc63bf79dd69e6e5803973df46076dfcd03911
shenweihai1 <shenweihai1@gmail.com>
2022-03-08 20:54:42 +0000

    updated

--- c0-single.sh
+++ c0-single.sh
@@ -7,4 +7,4 @@ let lock=trd/4
 
 echo "./bench_ycsb --logtostderr=1 --id=0 --servers='10.1.0.7:8000' --protocol=Calvin --partition_num=$trd --threads=$trd --batch_size=10000 --replica_group=3 --lock_manager=$lock --read_write_ratio=50 --cross_ratio=0 > ./xxxx14/$trd.log 2>&1"
 
-./bench_ycsb --logtostderr=1 --id=0 --servers="10.1.0.7:8000" --protocol=Calvin --partition_num=$trd --threads=$trd --batch_size=10000 --replica_group=1 --lock_manager=$lock --read_write_ratio=50 --cross_ratio=0 > ./xxxx14/$trd.log 2>&1
+./bench_ycsb --logtostderr=1 --id=0 --servers="127.0.0.1:8000" --protocol=Calvin --partition_num=$trd --threads=$trd --batch_size=10000 --replica_group=1 --lock_manager=$lock --read_write_ratio=50 --cross_ratio=0 > ./xxxx14/$trd.log 2>&1
